012340830db0bfd1d3acbe84e6debd5dfad11c92
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-24 22:25:29 +0530

    https://www.codechef.com/CCSTART2/problems/BUYPLSE

---
+++ buyplse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    short int a,b,c,d;
+    cin >> a >> b >> c >> d ;
+    cout << a*c+b*d;
+              
+ return 0;
+}
