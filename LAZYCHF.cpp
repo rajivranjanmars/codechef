d52dd9d8775ff918f49f6ccc8ca8c2271f0a9362
rajivranjanmars <rajivranjanmars@gmail.com>
2021-05-30 16:19:41 +0530

    https://www.codechef.com/START4C/problems/LAZYCHF

---
+++ LAZYCHF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){                 
+   ios_base::sync_with_stdio(false);
+cin.tie(NULL);                             
+    int t;
+    cin >> t; 
+    while (t--){
+       int x,m,d;
+       cin >> x >> m >> d;
+       if (x*m<=x+d)
+       cout<< x*m<<endl;
+       else
+       cout<< x+d<<endl;  
+    }            
+ return 0;
+}
